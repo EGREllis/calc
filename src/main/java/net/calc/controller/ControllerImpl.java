2ff1fe95cc0a219893da0d0ebc39581a29c467bf
egrellis <edwardellis.egre@gmail.com>
2020-01-24 12:42:48 +0000

    Implemented Plus/Minus

--- src/main/java/net/calc/controller/ControllerImpl.java
+++ src/main/java/net/calc/controller/ControllerImpl.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 
 public class ControllerImpl implements Controller {
     private static final String ALL_CLEAR = "AC";
+    private static final String PLUS_MINUS = "+/-";
     private static final String ZERO = "0";
     private static final Pattern DIGIT = Pattern.compile("[0-9]");
     private View view;
@@ -34,6 +35,14 @@ public class ControllerImpl implements Controller {
             }
         } else if (ALL_CLEAR.equals(label)) {
             display = "0";
+        } else if (PLUS_MINUS.equals(label)) {
+            if (display.equals(ZERO)) {
+                // Do nothing
+            } else if (display.charAt(0) == '-') {
+                display = display.substring(1);
+            } else {
+                display = "-"+display;
+            }
         }
         view.updateDisplay();
     }
